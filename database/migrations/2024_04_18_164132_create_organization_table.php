495ed8a189fb6e9aa6a329a4b87aacfec3f783f5
kandage <kandagehiruni@gmail.com>
2024-04-18 23:07:53 +0530

    updated migration files.

--- database/migrations/2024_04_18_164132_create_organization_table.php
+++ database/migrations/2024_04_18_164132_create_organization_table.php
@@ -9,13 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('organization', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   public function up(): void
+   {
+       Schema::create('organization', function (Blueprint $table) {
+           $table->id();
+           $table->string('name', 25);
+           $table->string('website', 45)->nullable();
+           $table->string('contact_person', 15)->nullable();
+           $table->string('contact_email', 15)->nullable();
+           $table->string('location', 45)->nullable();
+           $table->timestamps();
+       });
+   }
 
     /**
      * Reverse the migrations.
--- database/migrations/2024_04_18_165418_create_opportunity_table.php
+++ database/migrations/2024_04_18_165418_create_opportunity_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('opportunity', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 25);
+            $table->text('description')->nullable();
+            $table->enum('type', ['in_person', 'virtual'])->nullable();
+            $table->enum('category', ['education', 'environment', 'health', 'social_services'])->nullable();
+            $table->string('location', 35);
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->string('time_commitment', 25);
+            $table->text('skills_required')->nullable();
+            $table->unsignedBigInteger('org_id');
+            $table->foreign('org_id')->references('id')->on('organization')->onDelete('cascade');
             $table->timestamps();
         });
     }
