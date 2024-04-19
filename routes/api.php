4c207836f0ee2df414ddda6d4c714ec5eb8437cd
kandage <kandagehiruni@gmail.com>
2024-04-19 23:00:22 +0530

    updated routing.

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,30 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Opportunity routes
+Route::get('opportunities', 'OpportunityController@index');
+Route::get('opportunities/{opportunity}', 'OpportunityController@show');
+Route::post('opportunities', 'OpportunityController@store');
+Route::put('opportunities/{opportunity}', 'OpportunityController@update');
+Route::delete('opportunities/{opportunity}', 'OpportunityController@destroy');
 
-//Ro
+// Organization routes
+Route::get('organizations', 'OrganizationController@index');
+Route::get('organizations/{organization}', 'OrganizationController@show');
+Route::post('organizations', 'OrganizationController@store');
+Route::put('organizations/{organization}', 'OrganizationController@update');
+Route::delete('organizations/{organization}', 'OrganizationController@destroy');
 
-Route::get('');
+// User routes
+Route::get('users', 'UserController@index');
+Route::get('users/{user}', 'UserController@show');
+Route::post('users', 'UserController@store');
+Route::put('users/{user}', 'UserController@update');
+Route::delete('users/{user}', 'UserController@destroy');
 
-Route::get('');
-
-Route::put('');
-
-Route::delete('');
+// Volunteer Registration routes
+Route::get('volunteer-registrations', 'VolunteerRegistrationController@index');
+Route::get('volunteer-registrations/{volunteerRegistration}', 'VolunteerRegistrationController@show');
+Route::post('volunteer-registrations', 'VolunteerRegistrationController@store');
+Route::put('volunteer-registrations/{volunteerRegistration}', 'VolunteerRegistrationController@update');
+Route::delete('volunteer-registrations/{volunteerRegistration}', 'VolunteerRegistrationController@destroy');
