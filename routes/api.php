545d1c71da16b12b83425a28703ee580c2b33a1a
kandage <kandagehiruni@gmail.com>
2024-04-20 21:27:14 +0530

    updated routing.

--- routes/api.php
+++ routes/api.php
@@ -18,29 +18,30 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 // Opportunity routes
-Route::get('opportunities', 'OpportunityController@index');
-Route::get('opportunities/{opportunity}', 'OpportunityController@show');
-Route::post('opportunities', 'OpportunityController@store');
-Route::put('opportunities/{opportunity}', 'OpportunityController@update');
-Route::delete('opportunities/{opportunity}', 'OpportunityController@destroy');
+Route::get('opportunities',[App\Http\Controllers\OpportunityController::class, 'index']);
+Route::get('opportunities/{opportunity}',[App\Http\Controllers\OpportunityController::class, 'show']);
+Route::post('opportunities',[App\Http\Controllers\OpportunityController::class, 'store']);
+Route::put('opportunities/{opportunity}',[App\Http\Controllers\OpportunityController::class, 'update']);
+Route::delete('opportunities/{opportunity}',[App\Http\Controllers\OpportunityController::class, 'destroy']);
 
 // Organization routes
-Route::get('organizations', 'OrganizationController@index');
-Route::get('organizations/{organization}', 'OrganizationController@show');
-Route::post('organizations', 'OrganizationController@store');
-Route::put('organizations/{organization}', 'OrganizationController@update');
-Route::delete('organizations/{organization}', 'OrganizationController@destroy');
+Route::get('organizations',[App\Http\Controllers\OrganizationController::class, 'index']);
+Route::get('organizations/{organization}',[App\Http\Controllers\OrganizationController::class, 'show']);
+Route::post('organizations',[App\Http\Controllers\OrganizationController::class, 'store']);
+Route::put('organizations/{organization}',[App\Http\Controllers\OrganizationController::class, 'update']);
+Route::delete('organizations/{organization}',[App\Http\Controllers\OrganizationController::class, 'destroy']);
 
 // User routes
-Route::get('users', 'UserController@index');
-Route::get('users/{user}', 'UserController@show');
-Route::post('users', 'UserController@store');
-Route::put('users/{user}', 'UserController@update');
-Route::delete('users/{user}', 'UserController@destroy');
+Route::get('users',[App\Http\Controllers\UserController::class, 'index']);
+Route::get('users/{user}',[App\Http\Controllers\UserController::class, 'show']);
+Route::post('users',[App\Http\Controllers\UserController::class, 'store']);
+Route::put('users/{user}',[App\Http\Controllers\UserController::class, 'update']);
+Route::delete('users/{user}',[App\Http\Controllers\UserController::class, 'destroy']);
+
 
 // Volunteer Registration routes
-Route::get('volunteer-registrations', 'VolunteerRegistrationController@index');
-Route::get('volunteer-registrations/{volunteerRegistration}', 'VolunteerRegistrationController@show');
-Route::post('volunteer-registrations', 'VolunteerRegistrationController@store');
-Route::put('volunteer-registrations/{volunteerRegistration}', 'VolunteerRegistrationController@update');
-Route::delete('volunteer-registrations/{volunteerRegistration}', 'VolunteerRegistrationController@destroy');
+Route::get('volunteer-registrations', [App\Http\Controllers\VolunteerRegistrationController::class, 'index']);
+Route::get('volunteer-registrations/{volunteerRegistration}', [App\Http\Controllers\VolunteerRegistrationController::class, 'show']);
+Route::post('volunteer-registrations', [App\Http\Controllers\VolunteerRegistrationController::class, 'store']);
+Route::put('volunteer-registrations/{volunteerRegistration}', [App\Http\Controllers\VolunteerRegistrationController::class, 'update']);
+Route::delete('volunteer-registrations/{volunteerRegistration}', [App\Http\Controllers\VolunteerRegistrationController::class, 'destroy']);
